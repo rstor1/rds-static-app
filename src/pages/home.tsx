ea75dca25f2d1f13d4233e294a47f9d93d102456
Ryan <rstorm@gmail.com>
2025-06-09 20:35:58 -0400

    working on welcome and flex

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,33 +14,45 @@ const Home: FC = () => {
   const handlePause = () => {
     audioRef.current?.pause();
   };
-  
-  return (
-    <div className="home-container">
-      <header className="home-header"></header>
-      <main className="home-main">
-        <div className="top-left-link-container">
-          <Link to="/about" className="about-circle-link">
-            A
-          </Link>
-        </div>
-        <h1>Welcome to the Home Page!</h1>
-      </main>
-      <footer className="home-footer">
-        <audio
-          ref={audioRef}
-          src="http://ice1.somafm.com/deepspaceone-128-mp3"
-          autoPlay
-        />
-        <button onClick={handlePlay} className="audio-play-btn" aria-label="Play Audio">
-          <CgPlayButton size={32} />
-        </button>
-        <button onClick={handlePause} className="audio-play-btn" aria-label="Pause Audio">
-          <CgPlayPause size={32} />
-        </button>
-      </footer>
+
+return (
+  <div className="home-container">
+    <header className="home-header"></header>
+    <div className="welcome-wrapper">
+      <section className="welcome-section">
+        <div className="dividerTop">&nbsp;</div>
+        <h2 className="welcomeTitle">Welcome</h2>
+        <div className="dividerMiddle">&nbsp;</div>
+        <p className="welcomeText">
+          Welcome to my personal node of experimentation and interaction. The design you see to the left is line-stitching, which creates parabolic curves using only straight lines drawn with JavaScript inspired by inked line art I did some years back. Have a look around and enjoy.<br />
+          <br />
+          Technology: React - Typescript - Vite - SCSS
+        </p>
+        <div className="dividerBottom">&nbsp;</div>
+      </section>
     </div>
-  );
+    <main className="home-main">
+      <div className="top-left-link-container">
+        <Link to="/about" className="about-circle-link">
+          A
+        </Link>
+      </div>
+    </main>
+    <footer className="home-footer">
+      <audio
+        ref={audioRef}
+        src="http://ice1.somafm.com/deepspaceone-128-mp3"
+        autoPlay
+      />
+      <button onClick={handlePlay} className="audio-play-btn" aria-label="Play Audio">
+        <CgPlayButton size={32} />
+      </button>
+      <button onClick={handlePause} className="audio-play-btn" aria-label="Pause Audio">
+        <CgPlayPause size={32} />
+      </button>
+    </footer>
+  </div>
+);
 };
 
 export default Home;
